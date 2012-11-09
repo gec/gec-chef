1952b87c8a23e843ba0d1ca743ec87af10ac5a24
devans <devans@greenenergycorp.com>
2012-11-09 13:55:31 -0500

    Added initialization for Reef database

---
+++ cookbooks/reefdb/recipes/default.rb
@@ -0,0 +1,13 @@
+
+execute "reefdb_init" do
+  command "sudo su postgres -c \"psql < /tmp/dev_postgres_init.sql\""
+  action :nothing
+end
+ 
+cookbook_file "/tmp/dev_postgres_init.sql" do
+  source "dev_postgres_init.sql"
+  mode 0755
+  owner "root"
+  group "root"
+  notifies :run, resources(:execute => "reefdb_init")
+end
