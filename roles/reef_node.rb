b7bc9b1f1288f9434d06d1d95e181bffcc5bcff7
devans <devans@greenenergycorp.com>
2012-11-10 11:13:22 -0500

    Added reef node role for running actual reef instances

---
+++ roles/reef_node.rb
@@ -0,0 +1,11 @@
+name "reef_node"
+description "For running reef software"
+all_env = [
+  "role[unix_box]",
+  "role[qpid_box]",
+  "role[psql_box]",
+  "role[java_haver]",
+  "recipe[reefdb]"
+]
+
+run_list(all_env)
