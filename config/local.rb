a7d9c0ff6d033be663df79e36597c6913f6938af
devans <devans@greenenergycorp.com>
2013-01-11 16:13:05 -0500

    Genericized some of the local config information

--- config/local.rb
+++ config/local.rb
@@ -1,6 +1,6 @@
 file_cache_path  "/tmp/chef-solo"
-cookbook_path    "/home/devans/chef-repo/cookbooks"
-role_path 	 "/home/devans/chef-repo/roles"
-log_level        :debug
+cookbook_path    "/var/gec-chef/cookbooks"
+role_path 	 "/var/gec-chef/roles"
+log_level        :info
 log_location     STDOUT
 ssl_verify_mode  :verify_none
